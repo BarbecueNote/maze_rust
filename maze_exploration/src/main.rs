4196e937887804e675fa99ce5417aeca5b9a8cbc
BarbecueNote <tbrothers076@gmail.com>
2023-12-17 18:09:07 +0100

    v2

--- maze_exploration/src/main.rs
+++ maze_exploration/src/main.rs
@@ -1,23 +1,22 @@
 mod maze;
-
-use maze::{Maze};
+use maze::Maze;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let maze = Arc::new(Mutex::new(Maze::new())); 
-    let explored = Arc::new(Mutex::new(Vec::new()));
+    let maze = Arc::new(Mutex::new(Maze::new()));
     let exit_found = Arc::new(Mutex::new(false));
     let start_nodes = maze.lock().unwrap().get_start_nodes();
 
     let mut handles = vec![];
     for start_node in start_nodes {
         let maze_clone = Arc::clone(&maze);
-        let explored_clone = Arc::clone(&explored);
         let exit_found_clone = Arc::clone(&exit_found);
 
         let handle = thread::spawn(move || {
-            maze_clone.lock().unwrap().explore(start_node, &explored_clone, &exit_found_clone);
+            let mut trace = Vec::new();
+            maze_clone.lock().unwrap().explore(&start_node, &mut trace, &exit_found_clone);
+            println!("Thread trace: {:?}", trace);
         });
 
         handles.push(handle);
@@ -26,6 +25,4 @@ fn main() {
     for handle in handles {
         handle.join().unwrap();
     }
-
-    println!("Explored nodes: {:?}", *explored.lock().unwrap());
 }
--- maze_exploration/src/maze.rs
+++ maze_exploration/src/maze.rs
@@ -1,11 +1,11 @@
 use std::sync::{Arc, Mutex};
+use std::thread;
 
 #[derive(Clone, Debug)]
 pub struct Node {
     pub label: String,
     pub connections: Vec<String>,
 }
-
 pub struct Maze {
     nodes: Vec<Node>,
     exits: Vec<String>, 
@@ -22,30 +22,25 @@ impl Maze {
             exits: vec!["C".to_string()],
         }
     }
-
     pub fn get_start_nodes(&self) -> Vec<Node> {
         vec![self.nodes[0].clone()]
     }
-
-    pub fn explore(&self, node: Node, explored: &Arc<Mutex<Vec<Node>>>, exit_found: &Arc<Mutex<bool>>) {
-        let mut explored_guard = explored.lock().unwrap();
-        if explored_guard.iter().any(|n| n.label == node.label) || *exit_found.lock().unwrap() {
+    pub fn explore(&self, start_node: &Node, trace: &mut Vec<String>, exit_found: &Arc<Mutex<bool>>) {
+        if *exit_found.lock().unwrap() {
             return; 
         }
-
-        explored_guard.push(node.clone());
-
-        if self.exits.contains(&node.label) {
+        trace.push(start_node.label.clone());
+        if self.exits.contains(&start_node.label) {
             *exit_found.lock().unwrap() = true;
             return;
         }
-
-        drop(explored_guard);
-
-        for next_label in node.connections.iter() {
+        for next_label in &start_node.connections {
             if let Some(next_node) = self.nodes.iter().find(|n| &n.label == next_label) {
-                self.explore(next_node.clone(), explored, exit_found);
+                if !trace.contains(&next_node.label) {
+                    self.explore(next_node, trace, exit_found);
+                }
             }
         }
+        thread::yield_now(); 
     }
 }
